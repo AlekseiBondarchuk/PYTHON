1d78acca9af03c76426184261287622107a62045
AlekseiBondarchuk <125999487+AlekseiBondarchuk@users.noreply.github.com>
2023-06-01 14:58:31 +0300

    ex_7.py

--- LB_1/EX_7/ex_7.py
+++ LB_1/EX_7/ex_7.py
@@ -1,3 +1,3 @@
-IP = '192.168.3.1'
-res = ''.join([f'{x:10}' for x in IP.split('.')])+'\n'+ ' '.join(bin(int(x,16))[2:].zfill(9) for x in IP.split('.'))
-print(res)
+MAC = 'AAAA:BBBB:CCCC'
+MAC = ''.join(bin(int(x,16))[2:].zfill(8) for x in MAC.split(':'))
+print(MAC)
